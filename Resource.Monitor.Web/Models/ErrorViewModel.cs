5863c8bd0f379c5a9668b81289077f656d662bb7
Arpan Shah <arpanvgm@live.in>
2026-05-09 19:08:46 +0530

    init

---
+++ Resource.Monitor.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Resource.Monitor.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
